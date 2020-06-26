971a1a5b1601ded099c65dbaa24ab7a0b62c9f78
sobchenyuk <sobchenyuk@atri.tech>
2020-06-27 01:03:44 +0300

    exercise 2

--- index.js
+++ index.js
@@ -1,54 +1,23 @@
-//Задание 1
-class StringBuilder {
+const controlsBoxes = {
+    init() {
+        this.controls = document.querySelector('#controls');
+        this.boxes = document.querySelector('#boxes');
+        this.input = document.querySelector('.js-input');
 
-    constructor(baseString) {
-        this._value = baseString;
-    }
+        this.handler();
+    },
+    destroyBoxes: () => this.boxes.innerHTML = '',
+    createBoxes: (amount = '') => {
 
-    /**
-     * @param {string} str
-     * Метод append(str) - получает парметр str (строку)
-     * и добавляет ее в конец свойства value.
-     */
-    append(str) {
-        this._value = `${this._value}${str}`
-        return this;
-    }
+    },
+    handler() {
+        this.controls.addEventListener('click', (e) => {
+            const target = e.target;
+            const data = target.getAttribute('data-action');
 
-    /**
-     * @param {string} str
-     * Метод prepend(str) - получает парметр str (строку)
-     * и добавляет ее в начало свойства value.
-     */
-    prepend(str) {
-        this._value = `${str}${this._value}`
-        return this;
-    }
-
-    /**
-     * @param {string} str
-     * Метод pad(str) - получает парметр str (строку)
-     * и добавляет ее в начало и в конец свойства value.
-     */
-    pad(str) {
-        this._value = `${str}${this._value}${str}`
-        return this;
-    }
-
-    get value() {
-        return this._value;
-    }
-
-    set value(value) {
-        this._value = value;
+            !!data && this[`${data}Boxes`]()
+        })
     }
 }
 
-
-const builder = new StringBuilder('.');
-builder
-    .append('^')
-    .prepend('^')
-    .pad('=');
-
-console.log(builder.value);
+controlsBoxes.init()
