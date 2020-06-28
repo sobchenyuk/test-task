07fc4e9bd59d9afbcfc508f3e0ab85978851e14a
sobchenyuk <ablehands@ukr.net>
2020-06-28 13:20:34 +0300

    exercise 2

--- public/index.js
+++ public/index.js
@@ -3,36 +3,53 @@ const controlsBoxes = {
         this.controls = document.querySelector('#controls');
         this.boxes = document.querySelector('#boxes');
         this.input = document.querySelector('.js-input');
-        this.amount = 1;
         this.widthHeight = 30;
+        this.counter = 0;
+        this.rgbMaxColor = 225;
+        this.incrementParameter = 10;
+        this.defaultAmount = 1;
 
         this.handler();
     },
     destroyBoxes() {
+        /**
+         * @type {string}
+         * Функция destroyBoxes(), которая очищает div#boxes.
+         */
         this.boxes.innerHTML = ''
-
+        this.input.value = this.defaultAmount;
+        this.counter = 0;
     },
     createBoxes(amount) {
-        this.amount = Number(amount)
-        const count = 10 * this.boxes.childNodes.length;
-        const countWidthHeight = this.widthHeight + count;
-        const color = this.getRandomArbitrary(1)
-        const cssText = `width: ${countWidthHeight}px;height: ${countWidthHeight}px;background: rgb(${color}, ${color}, ${color});`;
+        /**
+         * @type {number}
+         * функция объявляет 1 параметр amount - число. Функция
+         * создает столько div, сколько указано в amount и добавляет их в div#boxes.
+         */
 
-        const div = document.createElement('div');
-        div.style.cssText = cssText;
+        const amountCount = Number(amount) + this.boxes.childNodes.length
+        while (this.counter  < amountCount) {
+            const color = this.getRandomArbitrary(1)
+            const count = this.counter * this.incrementParameter;
+            const countWidthHeight = this.widthHeight + count;
+            const cssText = `width: ${countWidthHeight}px;height: ${countWidthHeight}px;background: rgb(${color}, ${color}, ${color});`;
 
-        this.boxes.appendChild(div)
+            const div = document.createElement('div');
+            div.style.cssText = cssText;
+            this.boxes.appendChild(div)
+
+            this.counter++
+        }
     },
     getRandomArbitrary(min) {
-        return Math.random() * (255 - min) + min;
+        return Math.random() * (this.rgbMaxColor - min) + min;
     },
     handler() {
         this.controls.addEventListener('click', e => {
             const target = e.target;
             const data = target.getAttribute('data-action');
 
-            !!data && this[`${data}Boxes`](this.input.value || this.amount)
+            !!data && this[`${data}Boxes`](this.input.value || this.defaultAmount)
         })
     }
 }
